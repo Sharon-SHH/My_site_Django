c820a953122fd98ddfb5aafd399ed388752aad30
sharon-shh <sharonheng.shi@gmail.com>
2023-09-19 18:39:12 -0400

    move files

--- my_site/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('<slug:slug>', views.details, name="blog-detail") 
-    # path('<int:id>', views.details, name="blog-detail") 
-    #path('<str:title>', views.details) 
-]
--- my_site/monthly/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('<int:month>', views.monthly_digits),
-    path('<str:month>', views.index, name="month-direct"),
-    
-]
